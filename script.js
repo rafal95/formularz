741ee44e7e6733413d526537fa8e0266ea50a765
rafal95 <47984361+rafal95@users.noreply.github.com>
2019-07-18 15:58:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var slide = document.querySelector('.slide');
 var select = document.querySelector('.select');
-var option = document.querySelectorAll('li.first-level');
+var first_level = document.querySelectorAll('li.first-level');
+var two_level = document.querySelectorAll('.two-level');
+var thrid_level = document.querySelectorAll('.thrid-level');
 
-alert(option);
 function change() {
     var bullet = document.querySelector('.bullet');
     if (bullet.classList.contains("bullet-lft")) {
@@ -14,14 +15,61 @@ function change() {
     }
 }
 
-function rozwin(){
+function rozwin(event){
+    event.stopPropagation();
     var row = document.querySelector("#"+this.id+" div p");
     row.classList.toggle('more');
-    alert(row);
-    //row.style.color = "black";
+    var c = this.lastElementChild; 
+    if(c.nodeName == "UL"){
+        c.classList.toggle('hidden');
+        heigher_node = this.id;
+    }
+    if(c = document.querySelector("#"+this.id+" div img"))
+        c.classList.toggle("arrow");
+    else if(c = document.querySelector("#"+this.id+" div input")){
+        all = document.querySelector("#wszystkie")
+        if(c.id == "wszystkie"){
+            c = document.querySelectorAll("#"+heigher_node+" input");
+
+            if(all.checked)
+                all.checked = false;
+            else 
+                all.checked = true;
+
+            for(i=1;i<c.length;i++){
+                if(all.checked){
+                    if(!c[i].checked){
+                        c[i].previousElementSibling.classList.toggle("more");
+                        c[i].checked = true;
+                    }
+                }
+                else{
+                    c[i].checked = false;
+                    c[i].previousElementSibling.classList.toggle("more");;
+                }
+            }
+
+        }
+        else if(c.checked){
+            c.checked = false;
+            all.checked = false;
+            if(all.previousElementSibling.classList.contains("more"))
+                all.previousElementSibling.classList.toggle('more');
+        }
+        else
+            c.checked = true
+    }
 }
 
 slide.onclick = change;
-for(i=0;i<option.length;i++){
-    option[i].onclick = rozwin;
+for(i=0;i<first_level.length;i++){
+    first_level[i].onclick = rozwin;
+}
+
+for(i=0;i<two_level.length;i++){
+    two_level[i].onclick = rozwin;
+}
+
+for(i=0;i<thrid_level.length;i++){
+    thrid_level[i].onclick = rozwin;
 }
